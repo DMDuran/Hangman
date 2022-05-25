5c26e8d02ca84eb1f50d31a00e2a2389b3b4c739
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-05-25 16:48:53 -0500

    Created a loop that will add words to list that seperated by line in the .txt file.  Added a String called hiddenText that will chose a randomized word from the list.  Declaring 2 CharArrays that will allow array to check letters and record answers. Creating a boolean and a while loop to continue until game has ended as well as a loop inside to check for valid answers.

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -15,19 +15,54 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	//Creating main and making a throw exception just in case there is a error reading the file object
-	public static void main(String[] args) throws Exception  {
-		
-		//Creating file object to grab text information file
-		File Dictionary = new File("/Users/marcoduran/git/Hangman/Hangman/src/Dictionary.txt"); 
-		
-		//Creating scanner to allow us to read file
-		Scanner textScanner = new Scanner(Dictionary);
-		
-		//Creating an array list (string) for the words that are in dictionary 
-		ArrayList<String> words = new ArrayList<>();
-		
+	// Creating main and making a throw exception just in case there is a error
+	// reading the file object
+	public static void main(String[] args) throws Exception {
 
+		// Creating file object to grab text information file
+		File Dictionary = new File("/Users/marcoduran/git/Hangman/Hangman/src/Dictionary.txt");
+
+		Scanner textScanner = new Scanner(Dictionary); // Creating scanner to allow us to read file
+		Scanner input = new Scanner(System.in); // Creating scanner for user input
+
+		// Creating an array list (string) for the words that are in dictionary
+		ArrayList<String> words = new ArrayList<>();
+		// While loop theres a next line seperating the words, the scanner will continue
+		// to add words to list
+		while (textScanner.hasNextLine()) {
+			words.add(textScanner.nextLine());
+		}
+
+		// Creating a string that will randomize what word we get from list
+		String hidden_text = words.get((int) (Math.random() * words.size())); // Casting to int so we can use
+																				// math.random
+
+		// Now that we have the words, we need to change to a CharArray
+
+		// Declaring a CharArray
+		char[] textArray = hidden_text.toCharArray(); // Allows array to check letters
+		char[] myAnswers = new char[textArray.length]; // CharArray that will record answers
+
+		// Initializing answers with question marks
+		for (int i = 0; i < textArray.length; i++) {
+			myAnswers[i] = '?';
+		}
+
+		boolean finished = false;
+		// loop will continue to happen until game ends, finish == true
+		while (finished == true) {
+		System.out.println("****************************");
+		
+		String letter = input.next(); //User input
+		//Checks for valid input
+		while(letter.length() != 1 || Character.isDigit(letter.charAt(0))) {
+			System.out.println("Error Input - Try Again");
+			letter = input.next();
+		}
+		//Checks if letter is in the word
+		boolean found = false;
+		
+		}
 	}
 
 }
