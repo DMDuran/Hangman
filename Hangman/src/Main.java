bfff727e32935f6cf9ea7b6dad2cee603e0bb3bd
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-05-25 00:29:12 -0500

    Creating Hangman project as well as Main class.

---
+++ Hangman/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author marcoduran
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
