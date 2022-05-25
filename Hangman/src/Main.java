c70e8696e5fafaff43e040cec59a95160046b25d
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-05-25 00:51:05 -0500

    Added a txt file to project to create a dictionary.  Imported file our main class and creating an array list to read dictionary words.  Also imported a scanner for words.

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
 /**
  * 
  */
@@ -11,9 +15,19 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//Creating main and making a throw exception just in case there is a error reading the file object
+	public static void main(String[] args) throws Exception  {
 		
+		//Creating file object to grab text information file
+		File Dictionary = new File("/Users/marcoduran/git/Hangman/Hangman/src/Dictionary.txt"); 
+		
+		//Creating scanner to allow us to read file
+		Scanner textScanner = new Scanner(Dictionary);
+		
+		//Creating an array list (string) for the words that are in dictionary 
+		ArrayList<String> words = new ArrayList<>();
+		
+
 	}
 
 }
